97856bcf465125605e4ffcf8f6eb418e93346400
Mikko Lehtonen <mikko.lehtone@gmail.com>
2024-10-28 17:50:33 +0200

    Lisätty sum funktio

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+
+
+//Funktio laskee ahden luvun summan ja näyttää sen konsolissa
+function sum(a, b) {
+    return a + b;
+    }
+    console.log(sum(2, 3))
+    
+
 document.getElementById("message").innerText = "Hello, World!";
